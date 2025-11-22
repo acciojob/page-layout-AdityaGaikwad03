bac729d8ab0177c4ed23e65160bed84acc1013a4
AdityaGaikwad03 <gaikwadaditya0309@gmail.com>
2025-11-22 17:10:25 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const Header = (
+    <h1 className="text-5xl font-bold">Welcome to my website</h1>
+  );
+
+  const Footer = (
+    <p className="text-lg">© 2023 My Website. All rights reserved.</p>
+  );
   return (
     <div>
         {/* Do not remove the main div */}
+        <PageLayout header={Header} footer={Footer}>
+        <p className="text-xl mt-4">This is the content of my website.</p>
+      </PageLayout>
     </div>
   )
 }
